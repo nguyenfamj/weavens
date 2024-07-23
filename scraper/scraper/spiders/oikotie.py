b5cc02a0924a1f092bb551655ba534cb21906c21
Thang Phan <phancaothng@gmail.com>
2024-07-23 12:47:12 +0300

    feat: add oikotie spider

--- scraper/scraper/spiders/oikotie.py
+++ scraper/scraper/spiders/oikotie.py
@@ -1,10 +1,29 @@
-import scrapy
+from scrapy.http import Response
+from scrapy.loader import ItemLoader
+from scrapy_redis.spiders import RedisSpider
+
+from ..constants import TITLE_TO_FIELD
+from ..items import OikotieItem
 
 
-class OikotieSpider(scrapy.Spider):
+class OikotieSpider(RedisSpider):
     name = "oikotie"
     allowed_domains = ["asunnot.oikotie.fi"]
-    start_urls = ["https://asunnot.oikotie.fi"]
 
-    def parse(self, response):
-        pass
+    # Redis config
+    redis_key = "scraper:start_urls"
+    redis_batch_size = 10
+    max_idle_time = 7  # seconds
+
+    title_to_field = TITLE_TO_FIELD
+
+    def parse(self, response: Response):
+        il = ItemLoader(item=OikotieItem(), response=response)
+        il.add_value("url", response.url)
+
+        # Extract data from table
+        table_xpath = '//dt[text()="{title}"]/following-sibling::dd[1]//text()'
+        for title, field in self.title_to_field.items():
+            il.add_xpath(field, table_xpath.format(title=title))
+
+        return il.load_item()
