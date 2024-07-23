9ce7c84b7263fad713e977ac5c5ff3aefacf95d8
Thang Phan <phancaothng@gmail.com>
2024-07-23 17:33:00 +0300

    feat: add pipeline for saving scraped items to DynamoDB

--- scraper/scraper/pipelines.py
+++ scraper/scraper/pipelines.py
@@ -2,12 +2,31 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
+import boto3
 from itemadapter import ItemAdapter
 
 
-class ScraperPipeline:
+class DynamoDBPipeline:
+    def __init__(self, table_name, endpoint_url):
+        self.table_name = table_name
+        self.endpoint_url = endpoint_url
+        self.table = None
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        table_name = crawler.settings.get("DYNAMODB_TABLE_NAME")
+        endpoint_url = crawler.settings.get("DYNAMODB_ENDPOINT_URL")
+        return cls(table_name=table_name, endpoint_url=endpoint_url)
+
+    def open_spider(self, spider):
+        db = boto3.resource("dynamodb", endpoint_url=self.endpoint_url)
+        self.table = db.Table(self.table_name)
+
+    def close_spider(self, spider):
+        self.table = None
+
     def process_item(self, item, spider):
+        self.table.put_item(
+            Item={k: v for k, v in ItemAdapter(item).asdict().items() if v}
+        )
         return item
