975028343bb0d23cfd0e20917115a0a4636953c2
Thang Phan <phancaothng@gmail.com>
2024-07-30 17:02:37 +0300

    refactor: modify router prefix

--- backend/src/chat/router.py
+++ backend/src/chat/router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 from langserve import add_routes
 
+from ..config import settings
 from .agent import agent_executor
 
-router = APIRouter()
+router = APIRouter(prefix=settings.API_V1_STR)
 
 add_routes(router, agent_executor, path="/chat")
--- backend/src/main.py
+++ backend/src/main.py
@@ -7,8 +7,8 @@ from .properties.router import router as properties_router
 app = FastAPI(
     title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
 )
-app.include_router(properties_router, prefix=settings.API_V1_STR)
-app.include_router(chat_router, prefix=settings.API_V1_STR)
+app.include_router(properties_router)
+app.include_router(chat_router)
 
 
 @app.get("/healthcheck", include_in_schema=False)
