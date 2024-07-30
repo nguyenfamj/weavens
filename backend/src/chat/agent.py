0a19fbea67aa606f93f592ea900cf07f5a665020
Thang Phan <phancaothng@gmail.com>
2024-07-31 01:02:14 +0300

    feat: store chat history in DynamoDB instead of files

--- backend/src/chat/agent.py
+++ backend/src/chat/agent.py
@@ -1,57 +1,15 @@
-import re
-from pathlib import Path
-from typing import Callable, Union
-
-from fastapi import HTTPException
 from langchain.agents import AgentExecutor
 from langchain.agents.format_scratchpad import format_to_openai_functions
 from langchain.agents.output_parsers import OpenAIFunctionsAgentOutputParser
-from langchain_community.chat_message_histories import FileChatMessageHistory
-from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables.history import RunnableWithMessageHistory
 
 from .model import llm_with_tools
 from .schemas import Input, Output
+from .session import get_session_history
 from .tools import tools
 
-
-def _is_valid_identifier(value: str) -> bool:
-    """Check if the session ID is in a valid format."""
-    # Use a regular expression to match the allowed characters
-    valid_characters = re.compile(r"^[a-zA-Z0-9-_]+$")
-    return bool(valid_characters.match(value))
-
-
-def create_session_factory(
-    base_dir: Union[str, Path],
-) -> Callable[[str], BaseChatMessageHistory]:
-    """Create a session ID factory that creates session IDs from a base dir.
-
-    Args:
-        base_dir: Base directory to use for storing the chat histories.
-
-    Returns:
-        A session ID factory that creates session IDs from a base path.
-    """
-    base_dir_ = Path(base_dir) if isinstance(base_dir, str) else base_dir
-    if not base_dir_.exists():
-        base_dir_.mkdir(parents=True)
-
-    def get_chat_history(session_id: str) -> FileChatMessageHistory:
-        """Get a chat history from a session ID."""
-        if not _is_valid_identifier(session_id):
-            raise HTTPException(
-                status_code=400,
-                detail=f"Session ID `{session_id}` is not in a valid format. "
-                "Session ID must only contain alphanumeric characters, "
-                "hyphens, and underscores.",
-            )
-        file_path = base_dir_ / f"{session_id}.json"
-        return FileChatMessageHistory(str(file_path))
-
-    return get_chat_history
-
+CHAT_HISTORY_TABLE_NAME = "ChatHistories"
 
 prompt = ChatPromptTemplate.from_messages(
     [
@@ -83,7 +41,7 @@ agent_executor = AgentExecutor(agent=agent, tools=tools).with_config(agent_confi
 
 chat_with_history = RunnableWithMessageHistory(
     agent_executor,
-    create_session_factory("chat_histories"),
+    get_session_history(table_name=CHAT_HISTORY_TABLE_NAME),
     input_messages_key="human_input",
     history_messages_key="history",
 ).with_types(input_type=Input, output_type=Output)
--- backend/src/db.py
+++ backend/src/db.py
@@ -5,8 +5,9 @@ from .config import settings
 
 class DynamoDB:
     def __init__(self):
+        self.endpoint_url = settings.DYNAMODB_ENDPOINT_URL
         self.session = boto3.Session()
         self.resource = self.session.resource(
-            "dynamodb", endpoint_url=settings.DYNAMODB_ENDPOINT_URL
+            "dynamodb", endpoint_url=self.endpoint_url
         )
         self.table = self.resource.Table("oikotie_properties")
