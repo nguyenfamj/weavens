78e6f17d9977acd2d40c3ac684c627802383eb39
Thang Phan <phancaothng@gmail.com>
2024-07-31 16:35:48 +0300

    feat: init frontend

---
+++ frontend/app.py
@@ -0,0 +1,58 @@
+from uuid import uuid4
+
+import requests
+import streamlit as st
+
+st.set_page_config(page_title="Langchain App", page_icon="🦜", layout="wide")
+
+STARTING_MESSAGE = {
+    "role": "assistant",
+    "content": "Hello! How can I help you today?",
+}
+BACKEND_URL = "http://0.0.0.0:8686"
+
+
+# Side bar
+def generate_session_id():
+    session_id = str(uuid4())
+    st.session_state.session_id = session_id
+    st.session_state.messages = [STARTING_MESSAGE]
+
+
+st.sidebar.write("Session ID:")
+st.sidebar.code(st.session_state.get("session_id"))
+st.sidebar.button("Generate Session ID", on_click=generate_session_id)
+
+
+# App title
+st.title(":speaking_head_in_silhouette: Chatbot")
+
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [STARTING_MESSAGE]
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+if human_input := st.chat_input():
+    if not st.session_state.get("session_id"):
+        st.error("Please generate a session ID first.")
+        st.stop()
+    st.session_state.messages.append({"role": "user", "content": human_input})
+    with st.chat_message("user"):
+        st.write(human_input)
+    payload = {
+        "input": {
+            "human_input": human_input,
+        },
+        "config": {"configurable": {"session_id": st.session_state.session_id}},
+    }
+    with st.spinner("Processing..."):
+        response = requests.post(f"{BACKEND_URL}/api/v1/chat/invoke", json=payload)
+    message = (
+        response.json()
+        .get("output", {})
+        .get("output", "Sorry, there was an error processing your request.")
+    )
+    st.session_state.messages.append({"role": "assistant", "content": message})
+    st.chat_message("assistant").write(message)
