450459c014e336b602d9e5034184070f6d23af66
Thang Phan <phancaothng@gmail.com>
2024-07-31 14:18:15 +0300

    feat: add more filter to find the properties

--- backend/src/properties/schemas.py
+++ backend/src/properties/schemas.py
@@ -7,3 +7,15 @@ class PropertyQueryParams(BaseModel):
     max_price: int | None = Field(description="The maximum price of the property.")
     district: str | None = Field(description="The district of the property.")
     building_type: str | None = Field(description="The building type of the property.")
+    min_life_sq: int | None = Field(
+        description="The minimum living area of the property."
+    )
+    max_life_sq: int | None = Field(
+        description="The maximum living area of the property."
+    )
+    min_build_year: int | None = Field(
+        description="The minimum year the property was built."
+    )
+    max_build_year: int | None = Field(
+        description="The maximum year the property was built."
+    )
--- backend/src/properties/service.py
+++ backend/src/properties/service.py
@@ -24,6 +24,22 @@ class PropertyService:
             filter_expression = filter_expression & Attr("building_type").eq(
                 params.building_type
             )
+        if params.min_life_sq:
+            filter_expression = filter_expression & Attr("life_sq").gte(
+                params.min_life_sq
+            )
+        if params.max_life_sq:
+            filter_expression = filter_expression & Attr("life_sq").lte(
+                params.max_life_sq
+            )
+        if params.min_build_year:
+            filter_expression = filter_expression & Attr("build_year").gte(
+                params.min_build_year
+            )
+        if params.max_build_year:
+            filter_expression = filter_expression & Attr("build_year").lte(
+                params.max_build_year
+            )
 
         if filter_expression is None:
             response = db.table.scan()
