ddfb14743de0e6cd6f348115512ac57f8a979072
Thang Phan <phancaothng@gmail.com>
2024-08-23 12:59:26 +0300

    refactor: remove unused import

--- backend/src/chat/model.py
+++ backend/src/chat/model.py
@@ -1,16 +1,9 @@
-import os
-
-from dotenv import load_dotenv
 from langchain_core.utils.function_calling import convert_to_openai_function
 from langchain_openai import ChatOpenAI
 
 from .tools import tools
 
-load_dotenv()
-
-API_KEY = os.getenv("OPENAI_API_KEY")
-
 model = "gpt-4o-mini"
 model_config = dict(temperature=0.5, streaming=True)
-llm = ChatOpenAI(model=model, api_key=API_KEY, **model_config)
+llm = ChatOpenAI(model=model, **model_config)
 llm_with_tools = llm.bind(functions=[convert_to_openai_function(t) for t in tools])
