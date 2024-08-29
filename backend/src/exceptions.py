a13adb2e9f0820b8a0aaed9e153426ca2099f24e
Thang Phan <phancaothng@gmail.com>
2024-08-29 14:21:20 +0300

    refactor: move init method to parent

--- backend/src/exceptions.py
+++ backend/src/exceptions.py
@@ -7,15 +7,12 @@ class DetailHTTPException(HTTPException):
     STATUS_CODE: int
     DETAIL: dict[str, Any]
 
-    def __init__(self):
+    def __init__(self, detail: dict[str, Any] = None):
+        if detail:
+            self.DETAIL.update(detail)
         super().__init__(status_code=self.STATUS_CODE, detail=self.DETAIL)
 
 
 class NotFoundHTTPException(DetailHTTPException):
     STATUS_CODE = status.HTTP_404_NOT_FOUND
     DETAIL = {"message": "Not found"}
-
-    def __init__(self, detail: dict[str, Any] = None):
-        if detail:
-            self.DETAIL.update(detail)
-        super().__init__()
