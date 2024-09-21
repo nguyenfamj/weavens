1cd072e38af96c34a5cca3d25e5e4957db2d6591
Thang Phan <phancaothng@gmail.com>
2024-09-21 19:22:46 +0300

    refactor: use predefined modules and refactor self-made modules into production env ENG-43

--- backend/infra/modules/dynamodb/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "property_table_name" {
-  description = "The name of the DynamoDB table for storing property data"
-  type        = string
-}
-
-variable "checkpoint_table_name" {
-  description = "The name of the DynamoDB table for storing checkpoints"
-  type        = string
-}
