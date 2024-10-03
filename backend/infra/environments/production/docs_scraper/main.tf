1f186dd4a8547b5fce97f4a6ece80189fd4c2f5b
Thang Phan <phancaothng@gmail.com>
2024-10-03 17:29:39 +0300

    refactor: remove provider and locals ENG-48

--- backend/infra/environments/production/docs_scraper/main.tf
+++ backend/infra/environments/production/docs_scraper/main.tf
@@ -1,20 +1,8 @@
-provider "aws" {
-  region = local.aws_region
-
-  skip_metadata_api_check = true
-  skip_region_validation  = true
-}
-
-locals {
-  s3_bucket_name = "docs-scraper-lake"
-  aws_region     = "eu-north-1"
-}
-
 module "s3_bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "4.1.2"
 
-  bucket        = local.s3_bucket_name
+  bucket        = "docs-scraper-lake"
   request_payer = "BucketOwner"
 
   force_destroy = true
