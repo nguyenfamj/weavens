838f0949eebfb7117cf8709c483ac3c772103f2d
Thang Phan <phancaothng@gmail.com>
2024-10-06 17:02:49 +0300

    feat: add vpc and ec2 modules ENG-59

--- backend/infra/environments/production/main.tf
+++ backend/infra/environments/production/main.tf
@@ -37,3 +37,16 @@ module "ecr" {
 module "docs_scraper" {
   source = "./docs_scraper"
 }
+
+module "vpc" {
+  source = "./vpc"
+}
+
+module "ec2" {
+  source = "./ec2"
+
+  vpc_id                    = module.vpc.vpc_id
+  public_subnet_ids         = module.vpc.public_subnet_ids
+  private_subnet_ids        = module.vpc.private_subnet_ids
+  default_security_group_id = module.vpc.default_security_group_id
+}
