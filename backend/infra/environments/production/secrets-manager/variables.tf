f0f9ca0529496f6bb94f14e3b747966ccc6841d2
nguyenfamj <nguyenfamj1409@gmail.com>
2024-12-21 15:06:44 +0700

    feat: secrets_manager to manage app secrets

---
+++ backend/infra/environments/production/secrets-manager/variables.tf
@@ -0,0 +1,9 @@
+variable "BACKEND_OPENAI_API_KEY" {
+  type        = string
+  description = "The API key for the OpenAI API for the backend"
+}
+
+variable "BACKEND_FIRECRAWL_API_KEY" {
+  type        = string
+  description = "The API key for the FireCrawl API for the backend"
+}
