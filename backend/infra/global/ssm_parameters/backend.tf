fd60ec8378918c7b30230ba89746aa936e33034d
nguyenfamj <nguyenfamj1409@gmail.com>
2024-12-22 11:59:38 +0700

    feat: move from secrets_manager to ssm parameter store

--- backend/infra/global/ssm_parameters/backend.tf
+++ backend/infra/global/ssm_parameters/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-state-crux"
-    key            = "globalSecretsManager/terraform.tfstate"
+    key            = "globalSSMParameters/terraform.tfstate"
     region         = "eu-north-1"
     dynamodb_table = "TerraformStateLocks"
     encrypt        = true
---
+++ backend/infra/global/ssm_parameters/main.tf
@@ -0,0 +1,11 @@
+resource "aws_ssm_parameter" "production_openai_api_key" {
+  name  = "/crux/production/openai_api_key"
+  type  = "SecureString"
+  value = var.PRODUCTION_BACKEND_OPENAI_API_KEY
+}
+
+resource "aws_ssm_parameter" "production_firecrawl_api_key" {
+  name  = "/crux/production/firecrawl_api_key"
+  type  = "SecureString"
+  value = var.PRODUCTION_BACKEND_FIRECRAWL_API_KEY
+}
