71c21e2a13694c2540358d72fe33c57f6a8b1674
nguyenfamj <nguyenfamj1409@gmail.com>
2025-01-07 12:12:52 +0700

    refactor: move infra folder to repo root

--- backend/infra/environments/production/frontend/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "s3_bucket_name" {
-  value       = module.s3_bucket.s3_bucket_id
-  description = "Name of S3 bucket"
-}
--- backend/infra/global/ecr/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-crux"
-    key            = "globalECR/terraform.tfstate"
-    region         = "eu-north-1"
-    dynamodb_table = "TerraformStateLocks"
-    encrypt        = true
-  }
-}
