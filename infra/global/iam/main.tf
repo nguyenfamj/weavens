893065a31976f1fb3166590c18762fbfc7c18412
nguyenfamj <nguyenfamj1409@gmail.com>
2025-01-09 22:53:37 +0700

    fix: add missing ssm policy

--- infra/global/iam/main.tf
+++ infra/global/iam/main.tf
@@ -76,18 +76,105 @@ resource "aws_iam_policy" "github_actions_terraform_policy" {
           "dynamodb:GetItem",
           "dynamodb:PutItem",
           "dynamodb:DeleteItem",
+          "dynamodb:DescribeContinuousBackups",
+          "dynamodb:DescribeTable",
+          "dynamodb:DescribeTimeToLive",
+          "dynamodb:ListTagsOfResource",
           "ssm:GetParameter",
+          "ssm:GetParameters",
           "ssm:PutParameter",
+          "ssm:ListTagsForResource",
           "ecs:*",
-          "iam:PassRole"
+          "iam:PassRole",
+          "iam:GetPolicy",
+          "iam:GetPolicyVersion",
+          "iam:GetRole",
+          "iam:GetRolePolicy",
+          "iam:GetInstanceProfile",
+          "iam:ListRolePolicies",
+          "iam:ListAttachedRolePolicies",
+          "acm:DescribeCertificate",
+          "acm:ListTagsForCertificate",
+          "logs:DescribeLogGroups",
+          "logs:CreateLogGroup",
+          "logs:DeleteLogGroup",
+          "logs:ListTagsLogGroup",
+          "logs:ListTagsForResource",
+          "logs:PutRetentionPolicy",
+          "logs:TagLogGroup",
+          "logs:UntagLogGroup",
+          "es:DescribeDomain",
+          "es:DescribeElasticsearchDomain",
+          "es:DescribeElasticsearchDomainConfig",
+          "es:ListDomainNames",
+          "es:ListTags"
         ]
         Resource = [
           data.aws_s3_bucket.terraform_state.arn,
           "${data.aws_s3_bucket.terraform_state.arn}/*",
           data.aws_dynamodb_table.terraform_state_locks.arn,
           "arn:aws:ssm:${local.region}:${data.aws_caller_identity.current.account_id}:parameter/crux/production/*",
+          "arn:aws:ssm:${local.region}::parameter/*",
           "arn:aws:ecs:${local.region}:${data.aws_caller_identity.current.account_id}:*",
-          "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/*"
+          "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/*",
+          "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/*",
+          "arn:aws:iam::${data.aws_caller_identity.current.account_id}:instance-profile/*",
+          "arn:aws:acm:${local.region}:${data.aws_caller_identity.current.account_id}:certificate/*",
+          "arn:aws:logs:${local.region}:${data.aws_caller_identity.current.account_id}:log-group:*",
+          "arn:aws:es:${local.region}:${data.aws_caller_identity.current.account_id}:domain/*",
+          "arn:aws:dynamodb:${local.region}:${data.aws_caller_identity.current.account_id}:table/*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ssm:DescribeParameters",
+          "ec2:DescribeAvailabilityZones",
+          "ec2:DescribeVpcs",
+          "ec2:DescribeVpcAttribute",
+          "ec2:DescribeSubnets",
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeSecurityGroupRules",
+          "ec2:DescribeLaunchTemplates",
+          "ec2:DescribeLaunchTemplateVersions",
+          "elasticloadbalancing:DescribeLoadBalancers",
+          "elasticloadbalancing:DescribeTargetGroups",
+          "elasticloadbalancing:DescribeTargetGroupAttributes",
+          "elasticloadbalancing:DescribeTags",
+          "elasticloadbalancing:DescribeLoadBalancerAttributes",
+          "elasticloadbalancing:DescribeListeners",
+          "elasticloadbalancing:DescribeListenerAttributes",
+          "autoscaling:DescribeScalableTargets",
+          "autoscaling:DescribeAutoScalingGroups",
+          "autoscaling:DescribeLaunchConfigurations",
+          "autoscaling:DescribeLaunchTemplates",
+          "autoscaling:DescribeLaunchTemplateVersions",
+          "autoscaling:DescribeTags",
+          "application-autoscaling:DescribeScalableTargets",
+          "application-autoscaling:ListTagsForResource",
+          "application-autoscaling:DescribeScalingPolicies",
+          "application-autoscaling:DescribeScheduledActions",
+          "application-autoscaling:DescribeMetricCollectionTypes",
+          "application-autoscaling:DescribeScalingActivities"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ecs:RegisterTaskDefinition",
+          "ecs:DeregisterTaskDefinition",
+          "ecs:DescribeTaskDefinition",
+          "ecs:ListTaskDefinitions",
+          "ecs:ListTaskDefinitionFamilies",
+          "ecs:GetTaskDefinition",
+          "ecs:UpdateService",
+          "ecs:DescribeServices",
+          "ecs:CreateService",
+          "ecs:DeleteService"
+        ]
+        Resource = [
+          "arn:aws:ecs:${local.region}:${data.aws_caller_identity.current.account_id}:*"
         ]
       }
     ]
